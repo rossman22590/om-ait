9191b509b19285e207d2e938d9c977e9ff6dd708
Ross Cohen <rosscohen1990@gmail.com>
2025-08-08 23:05:09 -0400

    names

--- frontend/src/components/agents/custom-agents-page/marketplace-tab.tsx
+++ frontend/src/components/agents/custom-agents-page/marketplace-tab.tsx
@@ -66,7 +66,7 @@ export const MarketplaceTab = ({
           <SelectContent className='rounded-xl'>
             <SelectItem className='rounded-xl' value="all">All Agents</SelectItem>
             <SelectItem className='rounded-xl' value="mine">Mine</SelectItem>
-            <SelectItem className='rounded-xl' value="kortix">Kortix Verified</SelectItem>
+            <SelectItem className='rounded-xl' value="kortix">Machine  Verified</SelectItem>
             <SelectItem className='rounded-xl' value="community">Community</SelectItem>
           </SelectContent>
         </Select>
