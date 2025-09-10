bd3cf9779fe0151ab7a22d9091178457bd2cc678
Ross Cohen <rosscohen1990@gmail.com>
2025-09-09 21:49:20 -0400

    Change TOKEN_PRICE_MULTIPLIER from 2.0 to 1.5

--- backend/services/billing.py
+++ backend/services/billing.py
@@ -26,7 +26,7 @@ import json
 stripe.api_key = config.STRIPE_SECRET_KEY
 
 # Token price multiplier
-TOKEN_PRICE_MULTIPLIER = 2.0
+TOKEN_PRICE_MULTIPLIER = 1.5
 
 # Minimum credits required to allow a new request when over subscription limit
 CREDIT_MIN_START_DOLLARS = 0.20
