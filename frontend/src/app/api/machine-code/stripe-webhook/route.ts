6efd550c9422bf574fbbd1dd6ce609fbc2106b48
Ross Cohen <rosscohen1990@gmail.com>
2025-09-18 03:50:42 -0400

    Update route.ts

--- frontend/src/app/api/machine-code/stripe-webhook/route.ts
+++ frontend/src/app/api/machine-code/stripe-webhook/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-07-30.basil",
+  apiVersion: "2025-08-27.basil",
 });
 
 const webhookSecret = process.env.STRIPE_MACHINE_CODE_WEBHOOK!;
